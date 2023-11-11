3c7ebb1ee296f3035e1b02b056a9fb948faf6fa6
DongUk-Shin <az9170@naver.com>
2023-11-11 19:00:50 +0900

    MVC test

--- kim-spirng/src/main/java/kim/kimspirng/controller/KimController.java
+++ kim-spirng/src/main/java/kim/kimspirng/controller/KimController.java
@@ -3,6 +3,7 @@ package kim.kimspirng.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class KimController {
@@ -10,7 +11,13 @@ public class KimController {
     @GetMapping("hello")//URL 매칭
     public String hello(Model model) {
         //키 값
-        model.addAttribute("data", "hello!!dd!!!!!!!!");
+        model.addAttribute("data", "hello!");
         return "hello"; //템플릿 이름
+    }
+    @GetMapping("hello-mvc")//URL 매칭
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        //키 값
+        model.addAttribute("name", name);
+        return "hello-template"; //템플릿 이름
     }
 }
