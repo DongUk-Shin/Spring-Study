8608c083e72a2bc970548eba3ac184cb73921bbe
DongUk-Shin <az9170@naver.com>
2024-01-15 23:15:31 +0900

    빈 사이클 예시

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,33 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycle() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); //ConfigurableApplicationContext 는 close 해줘야 함
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("https://www.naver.com/");
+            return networkClient;
+            /*
+            스프링 빈은 다음과 같은 라이프 사이클을 가진다 
+            객체 생성 후 의존관계 주입
+            그래서 url이 지정이 안 됨
+             */
+        }
+    }
+}
