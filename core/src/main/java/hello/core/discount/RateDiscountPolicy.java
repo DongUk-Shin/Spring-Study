70776324cd8085c54c4fc1017ccd84354ab80e3e
DongUk-Shin <az9170@naver.com>
2024-01-15 21:36:17 +0900

    어노테이션 직접 생성해서 주입하기

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,7 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component //컴포넌트 스캔
 //@Qualifier("mainDiscountPolicy") 빈 별칭 -> 컴파일시 타입 체크가 안됨
-@Primary //우선순위 이거먼저
+//@Primary 우선순위 이거먼저
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
