3ab63a6799cc1713bcfdb7b9597e2dbcdc6f6f21
DongUk-Shin <az9170@naver.com>
2024-01-15 23:28:23 +0900

    빈 생명주기 - 인터페이스

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("url = " + url);
-        connect();
-        call("초기화 연결 메세지");
+        //connect();
+        //call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -25,5 +28,18 @@ public class NetworkClient {
     //서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지"); //의존 관계 주입 후 호출
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
