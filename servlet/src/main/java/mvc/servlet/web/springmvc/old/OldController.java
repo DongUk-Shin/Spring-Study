293c377e2ea246b4d872f962eae7a4e657d5e8a7
DongUk-Shin <az9170@naver.com>
2024-01-22 16:54:01 +0900

    핸들러 매핑, 어댑터 옛날 ver

---
+++ servlet/src/main/java/mvc/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,31 @@
+package mvc.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+@Component("/springmvc/old-controller") //스프링 빈의 이름 지정, 스프링 빈 이름으로 핸들러를 찾음 / 1. 핸들러 매핑
+public class OldController implements Controller {
+
+    /*
+    2. Controller 인터페이스를 실행 할 수 있는 핸들러 어댑터를 찾아야 한다
+    스프링은 이미 필요한 핸들러 매핑과 핸들러 어댑터를 거의 다 만들어 놓음 그래서 그냥 실행된다
+
+    핸들러 매핑 우선순위
+    0 = RequestMappingHandlerMapping : 애노테이션 기반의 컨트롤러인 @RequestMapping에서 사용
+    1 = BeanNameUrlHandlerMapping : 스프링 빈의 이름으로 핸들러를 찾는다. (현재 코드에서는 이거)
+
+    핸들러 어댑터 우선순위
+    0 = RequestMappingHandlerAdapter : 애노테이션 기반의 컨트롤러인 @RequestMapping에서 사용
+    1 = HttpRequestHandlerAdapter : HttpRequestHandler 처리
+    2 = SimpleControllerHandlerAdapter : Controller 인터페이스(애노테이션X, 과거에 사용) 처리 (현재 코드에서는 이거)
+     */
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
