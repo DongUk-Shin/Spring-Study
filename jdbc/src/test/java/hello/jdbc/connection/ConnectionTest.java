3e6150c9693b8333a178c5f87480dfac734b9ef1
DongUk-Shin <az9170@naver.com>
2024-01-26 01:46:26 +0900

    DriverManagerDataSource

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,47 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    /**
+     * DataSource 안 쓴 기존 방식
+     */
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection1={}", con1);
+        log.info("connection2={}", con2); //서로 다른 커넥션
+    }
+
+    /**
+     * DataSource "인터페이스" 를 통해서 커넥션을 가져옴 위와 거의 비슷
+     * 객체를 생성하는 시점에 파라미터를 넣는 것에서 차이가 있다 -> "설정과 사용의 분리"
+     */
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        // DriverManagerdataSource
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection1={}", con1);
+        log.info("connection2={}", con2);
+    }
+}
