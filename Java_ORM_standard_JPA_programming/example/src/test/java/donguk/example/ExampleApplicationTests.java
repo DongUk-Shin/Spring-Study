c8e6161a962adaeb257808dcb435bf58fa3202d0
DongUk-Shin <az9170@naver.com>
2024-02-01 20:11:00 +0900

    프로젝트 생성

---
+++ Java_ORM_standard_JPA_programming/example/src/test/java/donguk/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package donguk.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
